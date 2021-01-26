39fc6b6d0b24d963f7ddfa9984995c2b4452e216
Ayrat <ayrat1@mail.ru>
2021-01-27 00:06:50 +0300

    add get requests

---
+++ src/main/java/de/controllers/PeopleController.java
@@ -0,0 +1,35 @@
+package de.controllers;
+
+import de.dao.PersonDao;
+import de.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+    private final PersonDao personDao;
+
+    @Autowired
+    public PeopleController(PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("index", personDao.index());
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personDao.show(id));
+        return "people/show";
+    }
+}
