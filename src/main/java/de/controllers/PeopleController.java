a252da6d7a6e85b6b34b3877139addbf096ad8ab
Ayrat <ayrat1@mail.ru>
2021-01-28 22:35:08 +0300

    add validation

--- src/main/java/de/controllers/PeopleController.java
+++ src/main/java/de/controllers/PeopleController.java
@@ -2,9 +2,11 @@ package de.controllers;
 
 import de.dao.PersonDao;
 import de.models.Person;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -35,7 +37,10 @@ public class PeopleController {
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person) {
+    public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "people/new";
+        }
         personDao.save(person);
         return "redirect:/people";
     }
@@ -47,7 +52,10 @@ public class PeopleController {
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult, @PathVariable("id") int id) {
+        if (bindingResult.hasErrors()) {
+            return "people/edit";
+        }
         personDao.update(id, person);
         return "redirect:/people";
     }
--- src/main/java/de/dao/PersonDao.java
+++ src/main/java/de/dao/PersonDao.java
@@ -13,10 +13,10 @@ public class PersonDao {
 
     {
         people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, "Tom"));
-        people.add(new Person(++PEOPLE_COUNT, "Bob"));
-        people.add(new Person(++PEOPLE_COUNT, "Mike"));
-        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+        people.add(new Person(++PEOPLE_COUNT, "Tom", 10, "tom@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob", 20, "bob@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike", 30, "mike@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy", 40, "katy@mail.ru"));
     }
 
     public List<Person> index() {
@@ -36,6 +36,8 @@ public class PersonDao {
         Person personToBeUpdated = show(id);
 
         personToBeUpdated.setName(updatedPerson.getName());
+        personToBeUpdated.setAge(updatedPerson.getAge());
+        personToBeUpdated.setEmail(updatedPerson.getEmail());
     }
 
     public void delete(int id) {
